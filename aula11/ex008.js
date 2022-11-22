62e1641a8f7f5a9da47b9dd2fd17961b67cc1a6a
Felipe Pinheiro <113048688+FelipePinheiroRegina@users.noreply.github.com>
2022-11-21 23:46:14 -0300

    seguimos

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if ( vel > 60) {
+    console.log('Voce ultrapassou o excesso de velocidade, MULTADO! Valor da multa R$200.00.')
+}
+console.log('Use cinto de segurança, e tenha uma boa VIAGEM!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Estados Unidos da America'
+console.log(`Vivendo no ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro, pra cima do HEXA!') 
+}else {
+    console.log('Você é Estrangeiro!')
+}
