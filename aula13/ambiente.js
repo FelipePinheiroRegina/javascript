0c23347aa479096bdc34c0b8169ce0f517670e98
Felipe Pinheiro <113048688+FelipePinheiroRegina@users.noreply.github.com>
2022-11-28 21:36:13 -0300

    estrutura de repetição for

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,5 +1,12 @@
+/*
 var c = 10
 do{
     console.log(`passo ${c}`)
     c--
 } while(c > 0)
+*/
+var c = 10
+while(c > 0){
+    console.log(`passo ${c}`)
+    c--
+}
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai começar')
+for(c = 1; c <= 4; c++){
+    console.log(`Passo ${c}`)
+}
+console.log('Fim...')
