8facaec6da0488509c039e50aef62bc7d0dbc472
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-01-24 23:11:21 -0300

    projeto 01

---
+++ projetojs/javascript/script.js
@@ -0,0 +1,10 @@
+let blocotab = window.document.getElementById('tab')
+let blococont = window.document.getElementById('cont')
+let body = window.document.getElementById('ola')
+
+
+blocotab.addEventListener('click', Tabuada())
+function Tabuada(){
+    blocotab.style.display = 'block'
+}
+
